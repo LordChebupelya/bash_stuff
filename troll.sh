d83e0def8b0e7b062e89689131cf3a7fcb26f08b
LordChebupelya <111064437+LordChebupelya@users.noreply.github.com>
2023-03-14 14:02:55 +0300

    Create troll.sh

---
+++ troll.sh
@@ -0,0 +1,26 @@
+clear
+
+((t = 0))
+
+for t in {0..100}
+do
+  echo "Hacking Pentagon... &t%"
+  ((t = t + 1))
+  sleep 0.1s
+done
+echo "Hacking Pentagon... Done."
+
+clear
+
+((t = 0))
+
+for t in {0..100}
+do
+  echo "Retrieving data from Pentagon... &t%"
+  ((t = t + 1))
+  sleep 0.1s
+done
+echo "trolled haha" > pentagon_data_1.txt
+echo "You've been trolled You've been trolled" > pentagon_data_2.txt
+echo "get trolled" > pentagon_data_3.txt
+echo "Retrieving data from Pentagon... Done."
